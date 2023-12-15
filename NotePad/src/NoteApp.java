fe4bacf3ec1dc102f94e9dab92ca3d2a58037050
ShambaC <38806897+ShambaC@users.noreply.github.com>
2023-12-15 19:07:12 +0530

    Added font dialog box

--- NotePad/src/NoteApp.java
+++ NotePad/src/NoteApp.java
@@ -5,12 +5,15 @@ import java.nio.file.Files;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -18,13 +21,51 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.UIManager;
+
+// class for font settings window
+class FontWindow extends JFrame implements ActionListener {
+    int fontSize;
+    String fontFace;
+
+    public FontWindow(int fontSize, String fontFace) {
+        setTitle("Font Settings");
+        setSize(500, 400);
+        setBackground(Color.DARK_GRAY);
+        setForeground(Color.WHITE);
+        setDefaultCloseOperation(HIDE_ON_CLOSE);
+        this.fontSize = fontSize;
+        this.fontFace = fontFace;
+        initCmp();
+    }
+
+    private void initCmp() {
+        JPanel mainPanel = new JPanel();
+        setLayout(new FlowLayout());
+
+        JLabel sizeLabel = new JLabel("Size: ");
+        JTextField sizeField = new JTextField(3);
+        sizeField.setText(Integer.toString(fontSize));
+
+        mainPanel.add(sizeLabel);
+        mainPanel.add(sizeField);
+
+        add(mainPanel);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String cmd = e.getActionCommand();
+    }
+}
 
 public class NoteApp extends JFrame implements ActionListener {
 
     // GLobals
     JTextArea tArea;
     JMenuItem lwrapIt;
+    FontWindow fw;
 
     // Main Window
     public NoteApp() {
@@ -34,6 +75,8 @@ public class NoteApp extends JFrame implements ActionListener {
         setForeground(Color.WHITE);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         initCmp();
+        Font font = tArea.getFont();
+        fw = new FontWindow(font.getSize(), font.getName());
     }
 
     private void initCmp() {
@@ -57,6 +100,7 @@ public class NoteApp extends JFrame implements ActionListener {
         JMenuItem saveIt = new JMenuItem("Save");
         JMenuItem exitIt = new JMenuItem("Exit");
         // Under Format menu
+        JMenuItem font = new JMenuItem("Font");
         lwrapIt = new JMenuItem("Line Wrap");
 
         // Add listener
@@ -65,13 +109,15 @@ public class NoteApp extends JFrame implements ActionListener {
         saveIt.addActionListener(this);
         exitIt.addActionListener(this);
 
+        font.addActionListener(this);
         lwrapIt.addActionListener(this);
 
         file.add(newIt);
         file.add(openIt);
         file.add(saveIt);
         file.add(exitIt);
-
+        
+        format.add(font);
         format.add(lwrapIt);
 
         mb.add(file);
@@ -145,6 +191,9 @@ public class NoteApp extends JFrame implements ActionListener {
                 tArea.setLineWrap(true);
                 lwrapIt.setText("Line Wrap    ✔");
             }
+        }
+        else if(cmd.equalsIgnoreCase("Font")) {
+            fw.setVisible(true);
         }
     }
 
