886950cae251c4a1510da36187e2ab4d28cb9f75
ShambaC <38806897+ShambaC@users.noreply.github.com>
2024-01-17 00:02:33 +0530

    commented socket program

--- OnlineGDB/src/client.java
+++ OnlineGDB/src/client.java
@@ -1,3 +1,9 @@
+/*
+ * -------- BIG GOTCHA
+ *      
+ * This program will only work for a single client, multiple clients won't work.
+ */
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
@@ -12,34 +18,47 @@ import javax.swing.JFrame;
 
 public class client extends JFrame{
     public static void main(String[] args) throws Exception {
+        // Use JFileChooser to select the C program file to be compiled
         JFileChooser fc = new JFileChooser();
 
+        // Open the open dialog box
         int res = fc.showOpenDialog(fc);
         File f = null;
 
+        // If file exists
         if(res == JFileChooser.APPROVE_OPTION) {
+            // Display name
             System.out.println("File chosen, name: " + fc.getSelectedFile().getName());
 
+            // Refer to the file
             f = fc.getSelectedFile();
         }
 
+        // Open a socket and connect to the server
         Socket clientSocket = new Socket("localhost", 9000);
+        // Create an output stream to the server
         OutputStream outToServer = clientSocket.getOutputStream();
+        // Write the file to the server
         outToServer.write(Files.readAllBytes(f.toPath()));
+        // Close the stream and automatically close the socket to stop sending packets to the server
         outToServer.close();
         clientSocket.close();
 
         System.out.println("Compiling ....");
         System.out.println("Result: \n");
 
+        // Connect to the server again
         clientSocket = new Socket("localhost", 9000);
 
+        // Get data from server (the result)
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         String line;
+        // Display result from console
         while((line = inFromServer.readLine()) != null) {
             System.out.println(line);
         }
 
+        // Close connection
         clientSocket.close();
     }
 }
--- OnlineGDB/src/server.java
+++ OnlineGDB/src/server.java
@@ -1,3 +1,9 @@
+/*
+ * -------- BIG GOTCHA
+ *      
+ * This program will only work for a single client, multiple clients won't work.
+ */
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
@@ -11,27 +17,39 @@ import java.nio.file.StandardCopyOption;
 
 public class server {
     public static void main(String[] args) throws Exception {
+        // Constants
         String fileName = "../build/code.c";
         String compileString = "gcc ../build/code.c -o ../build/codeBin.exe";
         String runString = "../build/codeBin";
 
+        // Create a new socket at port 9000
         ServerSocket rootSocket = new ServerSocket(9000);
 
         while(true) {
             System.out.println("Server Started -----");
+            // Wait for an incoming connection
             Socket connectionSocket = rootSocket.accept();
 
+            // Create new file for the program
             File f = new File(fileName);
+            // Get file from incoming connection and save it to the already created file replacing its bits
             Files.copy(connectionSocket.getInputStream(), f.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            // Close this connection as the client closes connection after sending file.
             connectionSocket.close();
 
+            // Accept new connection from client
             connectionSocket = rootSocket.accept();
 
+            // Get the runtime to execute system commands
             Runtime rt = Runtime.getRuntime();
 
+            // Compile the C program using gcc by running the appropriate command
             Process process = rt.exec(compileString.split(" "));
 
             String line;
+            // Open a new output stream to client
+            OutputStream outToClient = connectionSocket.getOutputStream();
+            // Check if the compilation generates any error and send them to the client
             BufferedReader reader = new BufferedReader(
                     new InputStreamReader(process.getErrorStream()));
             while ((line = reader.readLine()) != null) {
@@ -40,8 +58,9 @@ public class server {
             reader.close();
             line = "";
 
+            // Run the built binary for the C program
             process = rt.exec(runString.split(" "));
-            OutputStream outToClient = connectionSocket.getOutputStream();
+            // Send the result after execution to the client
             reader = new BufferedReader(
                     new InputStreamReader(process.getInputStream()));
             while ((line = reader.readLine()) != null) {
@@ -49,6 +68,7 @@ public class server {
             }
             reader.close();
 
+            // Close the socket
             connectionSocket.close();
         }
     }
